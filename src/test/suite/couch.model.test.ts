9c0859c3c371a86e94f65e7e8a9ba41b8a6f2573
Artur Biko <artur.biko@fau.de>
2023-05-11 16:42:45 +0200

    Fix testcase for missing settings

--- src/test/suite/couch.model.test.ts
+++ src/test/suite/couch.model.test.ts
@@ -1,9 +1,20 @@
+import * as vscode from 'vscode';
 import { assert } from 'chai';
+import Sinon = require('sinon');
 import ConnectionService from '../../service/connection.service';
 import CouchModel from '../../provider/couch.model';
 
 describe('Test fetching of databases', () => {
-	const connection: ConnectionService = new ConnectionService();
+	let connection: ConnectionService;
+	before(() => {
+		const settings = {
+			get: (setting: string) => setting,
+		};
+
+		Sinon.mock(vscode.workspace).expects('getConfiguration').returns(settings);
+
+		connection = new ConnectionService();
+	});
 
 	it('Return empty set of databases.', () => {
 		const couch = new CouchModel(connection);
